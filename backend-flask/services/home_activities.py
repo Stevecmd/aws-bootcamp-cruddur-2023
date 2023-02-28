63d5e0d82e997658e525d86ceb90dcfce586d22d
Steve Murimi <murimi101@gmail.com>
2023-02-28 00:45:07 +0000

    Working Cruddur App

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -4,8 +4,8 @@ class HomeActivities:
     now = datetime.now(timezone.utc).astimezone()
     results = [{
       'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
-      'handle':  'Andrew Brown',
-      'message': 'Cloud is both interesting and fun!',
+      'handle':  'Steve Murimi',
+      'message': 'Cloud is both interesting and fun! A lot of fun!',
       'created_at': (now - timedelta(days=2)).isoformat(),
       'expires_at': (now + timedelta(days=5)).isoformat(),
       'likes_count': 5,
@@ -15,7 +15,7 @@ class HomeActivities:
         'uuid': '26e12864-1c26-5c3a-9658-97a10f8fea67',
         'reply_to_activity_uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
         'handle':  'Worf',
-        'message': 'This post has no honor!',
+        'message': 'This post has no honor! Nor the tidings of glory!',
         'likes_count': 0,
         'replies_count': 0,
         'reposts_count': 0,
@@ -24,8 +24,8 @@ class HomeActivities:
     },
     {
       'uuid': '66e12864-8c26-4c3a-9658-95a10f8fea67',
-      'handle':  'Worf',
-      'message': 'I am out of prune juice',
+      'handle':  'Taylor',
+      'message': 'I am out of prune juice, and I also need some MILK.',
       'created_at': (now - timedelta(days=7)).isoformat(),
       'expires_at': (now + timedelta(days=9)).isoformat(),
       'likes': 0,
@@ -33,7 +33,7 @@ class HomeActivities:
     },
     {
       'uuid': '248959df-3079-4947-b847-9e0892d1bab4',
-      'handle':  'Garek',
+      'handle':  'Andrew Brown',
       'message': 'My dear doctor, I am just simple tailor',
       'created_at': (now - timedelta(hours=1)).isoformat(),
       'expires_at': (now + timedelta(hours=12)).isoformat(),
