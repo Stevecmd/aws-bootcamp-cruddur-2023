a3098f717c4b6f624a6823e72ddeea239c3c4727
Steve Murimi <72811315+Stevecmd@users.noreply.github.com>
2023-03-27 00:24:55 +0300

    implement dynamodb into our application.

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -3,6 +3,7 @@ INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES
   ('Andrew Brown','andrew@rocketmail.com' , 'andrewbrown' ,'MOCK'),
   ('Andrew Bayko','bayko@rocketmail.com' , 'bayko' ,'MOCK');
+  ('Londo Mollari','lmollari@galaxynet.com' , 'londo' ,'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
--- backend-flask/services/message_groups.py
+++ backend-flask/services/message_groups.py
@@ -1,24 +1,27 @@
 from datetime import datetime, timedelta, timezone
+
+from lib.ddb import Ddb
+from lib.db import db
+# from lib.momento import MomentoCounter
+
 class MessageGroups:
-  def run(user_handle):
+  def run(cognito_user_id):
     model = {
       'errors': None,
       'data': None
     }
 
-    now = datetime.now(timezone.utc).astimezone()
-    results = [
-      {
-        'uuid': '24b95582-9e7b-4e0a-9ad1-639773ab7552',
-        'display_name': 'Andrew Brown',
-        'handle':  'andrewbrown',
-        'created_at': now.isoformat()
-      },
-      {
-        'uuid': '417c360e-c4e6-4fce-873b-d2d71469b4ac',
-        'display_name': 'Worf',
-        'handle':  'worf',
-        'created_at': now.isoformat()
-    }]
-    model['data'] = results
-    return model
+    sql = db.template('users','uuid_from_cognito_user_id') #find the handle from the uuid
+    my_user_uuid = db.query_value(sql,{
+      'cognito_user_id': cognito_user_id
+      }) # query value that returns a single uuid
+
+    print(f"UUID: {my_user_uuid}")
+
+    ddb = Ddb.client() #create a new client
+    data = Ddb.list_message_groups(ddb, my_user_uuid) #list out message groups while passing in uuid
+    print("list_message_groups:",data)
+
+    #MomentoCounter.reset(f"msgs/{user_handle}")
+    # model['data'] = data
+    # return model
