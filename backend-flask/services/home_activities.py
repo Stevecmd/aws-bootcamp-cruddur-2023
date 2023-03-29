89594c3e5e6dea7209a3c40a3b6f923417439737
Steve Murimi <murimi101@gmail.com>
2023-03-29 17:04:25 +0000

    Debugging, cruddur opens but dynamodb has an issue

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -44,10 +44,28 @@ class HomeActivities:
       
       
       
-      if param != None:
-      else:
+#       if param != None:
+#         else:
 
-      results.insert(0,)
-      span.set_attribute("app.result_length", len(results))
-      return results
+#       results.insert(0,)
+#       span.set_attribute("app.result_length", len(results))
+#       return results
     
+
+from datetime import datetime, timedelta, timezone
+from opentelemetry import trace
+
+from lib.db import db
+
+#tracer = trace.get_tracer("home.activities")
+
+class HomeActivities:
+  def run(cognito_user_id=None):
+    #logger.info("HomeActivities")
+    #with tracer.start_as_current_span("home-activites-mock-data"):
+    #  span = trace.get_current_span()
+    #  now = datetime.now(timezone.utc).astimezone()
+    #  span.set_attribute("app.now", now.isoformat())
+    sql = db.template('activities','home')
+    results = db.query_array_json(sql)
+    return results
