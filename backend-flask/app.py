babe16b54f0aea12ed024d6550e8cd388d89510b
Steve Murimi <72811315+Stevecmd@users.noreply.github.com>
2023-04-30 12:17:39 +0300

    removed health check version and moved a bit of code around

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -126,7 +126,7 @@ cors = CORS(
 
 @app.route('/api/health-check')
 def health_check():
-  return {'success': True, "ver": 1}, 200
+  return {'success': True}, 200
 
 # RollBar test
 # @app.route('/rollbar/test')
